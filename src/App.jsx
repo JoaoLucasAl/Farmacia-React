575551c485931eb9601fe6e2ead30cc8b63ee071
João Lucas Alberto Muniz <joaolucas.power@gmail.com>
2022-09-18 23:18:10 -0300

    :construction: Header homepage componente feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import HeaderHome from "./components/Homepage/Header/Header";
 
 function App() {
-  
-
   return (
     <div className="App">
+      {/* Homepage */}
+      <HeaderHome />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
