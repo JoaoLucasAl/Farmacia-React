5fec54d631c90525af3c0a346621f467fc2a4772
João Lucas Alberto Muniz <joaolucas.power@gmail.com>
2022-09-20 16:59:16 -0300

    :construction: build: Tela de cadastros em desenvolvimento

--- src/components/Cadastros/Main/Main.jsx
+++ src/components/Cadastros/Main/Main.jsx
@@ -1,13 +1,31 @@
-import React from "react";
-import './MainStyle.css'
+import React, { useEffect, useState } from "react";
+import "./MainStyle.css";
 
 const MainCadastro = () => {
-    return (
-        <div className="main_cadastro">
-            <div className="cadastro_div"></div>
+  const [ route, setRoute ] = useState("");
+
+
+
+  useEffect(() => console.log(route), [route])
+  return (
+    <div className="main_cadastro">
+      <div className="cadastro_div">
+        <div className="select_route_div">
+          <p>Rota:</p>
+          <select
+            className="select_route"
+            onChange={ (event) => { setRoute(event.target.value)}}
+          >
+          <option></option>
+            {["Clientes", "Funcionários", "Vendas", "Remedios"].map((item, index) => {
+              return <option key={index}>{item}</option>;
+            })}
+          </select>
         </div>
-    )
-}
+        <div className="input_data"></div>
+      </div>
+    </div>
+  );
+};
 
-
-export default MainCadastro
+export default MainCadastro;
