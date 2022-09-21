f339a4d7d4e78d9f49ceeaa7cbbcff09f8157ca3
isadora30 <oliveira.isadorasantana@gmail.com>
2022-09-21 15:30:09 -0300

    implementãcao do load, carregamento inicial.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeMain from "../components/Home/HomeMain/HomeMain";
 import SecondHeader from "../components/Home/SecondHeader/SecondHeader";
+import { Load } from "../components/Load/Load";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  setTimeout(()=> {
+    setLoading(false)
+  },2500)
+
+  if (loading) {
+    return <Load/>
+  }
+
   return (
     <div>
       <SecondHeader />
