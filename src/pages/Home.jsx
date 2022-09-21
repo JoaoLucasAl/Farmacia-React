671e8645ba729704339b8bf9bcc03606d00faffa
matheus <math99@outlook.com>
2022-09-21 00:37:00 -0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HomeMain from '../components/Home/HomeMain/HomeMain'
+import SecondHeader from '../components/Home/SecondHeader/SecondHeader'
+
+const Home = () => {
+  return (
+    <div>
+         <SecondHeader/>
+         <HomeMain/>
+    </div>
+  )
+  
+}
+
+export default Home
