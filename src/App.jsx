a53463896b008ce2a4007ec6f6648cc12bd46ed8
matheus <math99@outlook.com>
2022-09-21 00:37:00 -0300

    Implementado feature rooter nas páginas home e cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,28 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer.jsx";
 import SecondHeader from "./components/Home/SecondHeader/SecondHeader";
 import HomeMain from "./components/Home/HomeMain/HomeMain";
-
 import TitleCadastro from "./components/Cadastros/Title/Title";
 import MainCadastro from "./components/Cadastros/Main/Main";
+import Home from "./pages/Home";
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Cadastros from "./pages/Cadastros";
 function App() {
   return (
     <div className="App">
-      {/* Homepage */}
-      {/* <Header />
-      <SecondHeader />
-      <HomeMain />
-      <Footer /> */}
-
+      <BrowserRouter>
+      <Header/>
+     <Routes>
+    
+      <Route path="/" element ={<Home/>}/>
+      <Route path="/Cadastros" element ={<Cadastros/>}/>
+     </Routes>
+     <Footer/>
+     </BrowserRouter>
       {/* Cadastros */}
-      <Header />
+      {/* <Header />
       <TitleCadastro />
       <MainCadastro />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
